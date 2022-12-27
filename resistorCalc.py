e5948e865325f45db507d1a6d3ab133fbda9fbac
dpc2 <dpculler91@gmail.com>
2022-12-27 09:28:34 -0700

    Starting on series + parallel function

--- resistorCalc.py
+++ resistorCalc.py
@@ -33,7 +33,41 @@ def series():
         total += float(item)
     print('The series resistance is:\n{:.1f}\u03A9'.format(total))
 
-choice = input('Resistor Calculator!\nEnter S for Series or P for Parallel:\n')
+def both():
+    print('\Series/Parallel Network\nEnter resistor values, q to stop entry.')
+    comboSeries = list()
+    
+    comboParallel = list()
+    subParallel = list()
+    indexS = 0
+    indexP = 0
+
+    while True:
+        choice = input('\nSeries (S) or Parallel (P) sub network?\nq to quit\n')
+        if choice == 'S' or choice == 's':
+            subSeries = list()
+            while True:
+                entry = input("\nSeries resistor value:\n")
+                if entry != 'q':
+                    subSeries.append(entry)
+                    indexS =+ 1
+                elif entry == 'q':
+                    break
+            print(subSeries)
+            comboSeries.append(subSeries)
+            print(comboSeries)
+
+    print('Your resistor values:\n{}\n'.format(comboSeries))
+
+    total = 0
+    #for index, item in enumerate(values):
+        #total += float(item)
+    #print('The series resistance is:\n{:.1f}\u03A9'.format(total))
+
+
+
+
+choice = input('Resistor Calculator!\nEnter S for Series, P for Parallel, or B for both:\n')
 values = list()
 index = 0
 
@@ -43,5 +77,8 @@ if choice == 'S' or choice == 's':
 elif choice == 'P' or choice == 'p':
     print('You chose: parallel\n')
     parallel()
+elif choice == 'B' or choice == 'b':
+    print('You chose: both\n')
+    both()
 else:
     exit()
